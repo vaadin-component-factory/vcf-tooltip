a7e3e70ac8e4b17da93f9c55642a92e21715a1cc
Nii Yeboah <nii@vaadin.com>
2022-03-18 16:02:36 -0700

    v23.0.1

--- src/vcf-tooltip.js
+++ src/vcf-tooltip.js
@@ -126,7 +126,7 @@ class VcfTooltip extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '23.0.0';
+    return '23.0.1';
   }
 
   static get properties() {
