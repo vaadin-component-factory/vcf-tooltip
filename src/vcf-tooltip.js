7646b81e279484557f258c3ff285621d24a3599b
mukherjeesudebi <sudebi@vaadin.com>
2023-09-28 16:41:46 +0300

    updated versions for v22 compatibility

--- src/vcf-tooltip.js
+++ src/vcf-tooltip.js
@@ -126,7 +126,7 @@ export class Tooltip extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '23.0.5';
+    return '22.0.0';
   }
 
   static get properties() {
